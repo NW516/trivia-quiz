b3a24024bea37741cdc026ab42984f29d280df07
Nicole Wiegand <nwiegand05@yahoo.com>
2020-08-13 14:24:43 -0400

    Added styles using Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import QuestionCard from './components/QuestionCard';
 // Types
 import { QuestionState, Difficulty } from './API';
 
+import { GlobalStyle, Wrapper } from './App.styles';
+
 export type AnswerObject = {
   question: string;
   answer: string;
@@ -72,7 +74,9 @@ const App = () => {
   };
 
   return (
-    <div className="App">
+    <>
+    <GlobalStyle />
+    <Wrapper>
     <h1>REACT QUIZ</h1>
     {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
       <button className="start" onClick={startTrivia}>
@@ -96,7 +100,8 @@ const App = () => {
       Next Question
       </button>
      ) : null}
-    </div>
+    </Wrapper>
+    </>
   );
 };
 
